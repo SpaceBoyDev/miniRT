08920b2d7b335b306e7f207c72866adb80521327
Darío <dario.mart.lop.03@gmail.com>
2025-12-15 18:23:38 +0100

    fixed render.h norme error

--- minirt_bonus/include/render.h
+++ minirt_bonus/include/render.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   render.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alepinto <alepinto@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dario <dario@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/25 17:46:12 by dario             #+#    #+#             */
-/*   Updated: 2025/12/14 06:29:38 by alepinto         ###   ########.fr       */
+/*   Updated: 2025/12/15 18:22:56 by dario            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,12 +33,12 @@ t_hit			hit_hyperboloid(t_obj *obj, t_geo *geo, t_ray *r);
 void			init_quad(t_quad *q, t_vec3 d_perp, t_vec3 oc_perp, double r);
 t_hit			make_hit_cy(double t, t_vec3 p, t_vec3 n, t_cyl_data *data);
 void			update_best(t_hit *best, t_hit *candidate);
-t_hit			check_lateral_hit(t_ray *r, double t, \
-	double height, t_cyl_data *data);
-void			check_one_root(t_hit *best, t_ray *r, \
-	t_cyl_data *data, double t);
-t_hit			make_paraboloid_hit(t_pb_ctx *ctx, double t, \
-	t_vec3 p, t_vec3 n);
+t_hit			check_lateral_hit(t_ray *r, double t,
+					double height, t_cyl_data *data);
+void			check_one_root(t_hit *best, t_ray *r,
+					t_cyl_data *data, double t);
+t_hit			make_paraboloid_hit(t_pb_ctx *ctx, double t,
+					t_vec3 p, t_vec3 n);
 void			solve_quadratic(t_hit *best, t_pb_ctx *ctx, t_quad *q);
 t_quad			init_quadratic_pa(t_pb_ctx *ctx);
 
@@ -51,8 +51,8 @@ t_vec3			compute_cone_normal(t_vec3 p, t_cone_params *cp, double base_r);
 
 //hit_hyperboloid.c
 t_hyper_dots	init_hyper_dots(t_ray *r, t_vec3 oc, t_vec3 axis);
-t_hit			make_hyperboloid_hit(double t, t_vec3 p, \
-	t_vec3 n, t_hyper_params *hp);
+t_hit			make_hyperboloid_hit(double t, t_vec3 p,
+					t_vec3 n, t_hyper_params *hp);
 t_quadratic		get_hyper_quadratic(t_ray *r, t_hyperboloid *h);
 t_vec3			compute_hyper_normal(t_vec3 p, t_hyperboloid *h, t_vec3 axis);
 bool			check_hyper_height(t_vec3 p, t_hyper_params *hp);
